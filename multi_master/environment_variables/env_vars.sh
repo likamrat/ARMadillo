2399a02445fb3fb12f42b9cb7cf8ebae6f2f7091
likamrat <likamrat@microsoft.com>
2019-04-29 16:29:31 -0700

    cleanup

--- multi_master/environment_variables/env_vars.sh
+++ multi_master/environment_variables/env_vars.sh
@@ -4,7 +4,7 @@
 export  Pi_USERNAME=pi
 export  Pi_PASSWORD=raspberry
 
-# Network
+# Network parameters
 export  LOAD_BALANCER_HOSTNAME=<LB HOSTNAME HERE>
 export  MASTER01_HOSTNAME=<k8s_MASTER_01 HOSTNAME HERE>
 export  MASTER02_HOSTNAME=<k8s_MASTER_02 HOSTNAME HERE>
@@ -30,11 +30,16 @@ export  ST=WA
 # Follow kubernetes release notes for the current validated docker versions @ https://kubernetes.io/docs/setup/release/notes/ 
 export VERSION=18.09 
 
-# NFS Settings
+# NFS parameters
 export NFS_SERVER=<Pi IP HERE>                          # The Pi connected to the external disk
 export SERVER_NFS_MOUNT=<MOUNTPOINT HERE>               # The NFS mount point on the Pi acting as NFS server. Example "/mnt/extstorage/k8s_nfs"
 export CLIENT_NFS_PV=<PV CLIENT DIRECTORY HERE>         # This directory will act as the persistent volume mapped to the NFS mount on the NFS server. Example "$HOME/k8s_pv"
 export Pi_SUBNET=<Pi SUBNET HERE>                       # Can be changed to specific IP(s). For the ease of use, allow the entire Pi's subnet. Example: 172.16.1.0/24
+
+# rclone parameters
+export rclone_CONFIG_NAME = <rclone CONFIG NAME HERE>            # rclone configuration name                                             
+export AZURE_BLOB_ACCOUNT = <AZURE STORAGE ACCOUNT NAME HERE>    # Target Azure storage account name 
+export AZURE_BLOB_KEY = <AZURE STORAGE ACCOUNT KEY HERE>         # Target Azure storage account key
 
 # No need to change unless you have more then 3 masters and 2 workers
 export MASTERS_HOSTS="$MASTER01_HOSTNAME $MASTER02_HOSTNAME $MASTER03_HOSTNAME"
--- storage/nfs_storage/sync_data.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-curl https://rclone.org/install.sh | sudo bash
-
-rclone config create "armadillodata" azureblob --azureblob-account "armadillodata" --azureblob-key "0eLXiQ3QkegxejSdOU+VERtXc1CZmHmwte0c7FjYksZCGQTih+7wBBrswCmOW4YvCOkUpFsNGj7jCQBjGhrkPg=="
-
-rclone sync /mnt/extstorage/k8s-nfs armadillodata:pi-nfs-data --progress  
