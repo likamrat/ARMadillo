e641277dbce485973d1cd8c755113df17767913e
likamrat <likamrat@microsoft.com>
2019-11-07 16:28:56 -0800

    initial commit

--- scripts/git_clone_private.sh
+++ scripts/git_clone_private.sh
@@ -5,7 +5,7 @@
 sudo apt-get install git -qy
 
 export  github_token=<YOUR GITHUB ACCESS TOKEN HERE>    # Follow https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line
-export  github_repo=<YOUR ARMadillo REPO HERE>          # For example: github.com/your_github_username/ARMadillo.git (no need for https://)
+export  github_repo=<YOUR ARMadillo REPO HERE>          # For example: github.com/your_github_username/ARMadillo.git (no need to incl. "https://")
 export  dest_dir=<DESTINATION DIRECTORY HERE>           # For example: armadillo
 
 git clone https://$github_token@$github_repo $dest_dir
