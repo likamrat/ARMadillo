f19e7182fb0d90036094e4a1cb057205a746dcfc
likamrat <likamrat@microsoft.com>
2019-11-11 08:35:44 -0800

    initial commit

--- scripts/kubeadm_cleanup.sh
+++ scripts/kubeadm_cleanup.sh
@@ -9,17 +9,6 @@ sudo rm -f kubeadm-config.yaml
 sudo rm -f join_master.sh
 sudo rm -f join_worker.sh
 sudo rm -f kubeadm_run.log
-sudo rm -f ca.crt
-sudo rm -f ca.key
-sudo rm -f sa.key
-sudo rm -f sa.pub
-sudo rm -f front-proxy-ca.crt
-sudo rm -f front-proxy-ca.key
-sudo rm -f etcd-ca.crt
-sudo rm -f etcd-ca.key
-sudo rm -f admin.conf
-sudo rm -f join_master.sh
-sudo rm -f join_worker.sh
 sudo rm -f config
 docker rmi $(docker images --filter=reference="*weaveworks/weave*" -q)
 sudo rm -Rf ARMadillo 
