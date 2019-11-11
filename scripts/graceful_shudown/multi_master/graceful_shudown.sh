86cb8842104a75dd4febcd0e7a9695db016cca47
likamrat <likamrat@microsoft.com>
2019-11-11 11:23:23 -0800

    initial commit

--- scripts/graceful_shudown/multi_master/graceful_shudown.sh
+++ scripts/graceful_shudown/multi_master/graceful_shudown.sh
@@ -3,6 +3,12 @@
 # Source env vars
 source ARMadillo/deploy/multi_master/env_vars.sh
 
-for host in ${MASTERS_HOSTS} && ${WORKERS_HOSTS}; do
-    sudo sshpass -p $Pi_PASSWORD ssh -o StrictHostKeyChecking=no $Pi_USERNAME@$host 'sudo /sbin/shutdown -hP now'
+for host in ${WORKERS_HOSTS}; do
+    sshpass -p $Pi_PASSWORD ssh -o StrictHostKeyChecking=no $Pi_USERNAME@$host 'sudo /sbin/shutdown -hP now'
 done
+
+for host in ${MASTERS_HOSTS}; do
+    sshpass -p $Pi_PASSWORD ssh -o StrictHostKeyChecking=no $Pi_USERNAME@$host 'sudo /sbin/shutdown -hP now'
+done
+
+sshpass -p $Pi_PASSWORD ssh -o StrictHostKeyChecking=no $Pi_USERNAME@$LOAD_BALANCER_HOSTNAME 'sudo /sbin/shutdown -hP now'
--- scripts/graceful_shudown/single_master/graceful_shudown.sh
+++ scripts/graceful_shudown/single_master/graceful_shudown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Source env vars
-source ARMadillo/deploy/single_master/env_vars.sh
+source deploy/single_master/env_vars.sh
 
 for host in ${MASTERS_HOSTS} && ${WORKERS_HOSTS}; do
-    sudo sshpass -p $Pi_PASSWORD ssh -o StrictHostKeyChecking=no $Pi_USERNAME@$host 'sudo /sbin/shutdown -hP now'
+    sshpass -p $Pi_PASSWORD ssh -o StrictHostKeyChecking=no $Pi_USERNAME@$host 'sudo /sbin/shutdown -hP now'
 done
